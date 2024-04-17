ded300222d9d1ad404d861cad8f6c0d304f5c861
Maria Jenkins <macjenkin@gmail.com>
2024-04-17 10:08:24 -0500

    create User model

--- server/config.py
+++ server/config.py
@@ -4,8 +4,11 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from flask_bcrypt import Bcrypt
+import os
 
 app = Flask(__name__)
+app.secret_key = os.getenv("secret_key")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
@@ -15,7 +18,10 @@ metadata = MetaData(naming_convention={
 })
 db = SQLAlchemy(metadata=metadata)
 migrate = Migrate(app, db)
+
 db.init_app(app)
+
+bcrypt = Bcrypt(app)
 
 api = Api(app)
 
--- server/seed.py
+++ server/seed.py
@@ -3,9 +3,48 @@ from random import randint, choice as rc
 from faker import Faker
 
 from app import app
-from models import db
+from models import db, User
 
 if __name__ == '__main__':
-    fake = Faker()
+
     with app.app_context():
-        print("Seeding database...")
+
+        print('Deleting records...')
+        User.query.delete()
+
+        fake = Faker()
+
+        print('Creating users...')
+
+        users=[]
+        usernames = []
+
+        maria = User(username='Maria', admin=True)
+        maria.password_hash = 'chess'
+
+        usernames.append('Maria')
+        users.append(maria)
+
+        for i in range(20):
+
+            username = fake.first_name()
+
+            while username in usernames:
+
+                username = fake.first_name()
+
+            usernames.append(username)
+
+            user = User(
+                username=username,
+                admin=False
+            )
+
+            user.password_hash = user.username + 'password'
+
+            users.append(user)
+        
+        db.session.add_all(users)
+        db.session.commit()
+
+        print('Database seeded.')

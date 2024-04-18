0a60119966ec7ad9591bd9043588e52cdd1660af
Maria Jenkins <macjenkin@gmail.com>
2024-04-18 14:34:21 -0500

    add Users and UsersByID resources

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
-from flask import request, session, make_response
+from flask import jsonify, request, session, make_response
 from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
 
 from config import app, db, api
 from models import User
@@ -41,7 +42,6 @@ class CheckSession(Resource):
         
         return make_response({'error': '401 Unauthorized'}, 401)
 
-
 class Logout(Resource):
     
     def delete(self):
@@ -54,10 +54,96 @@ class Logout(Resource):
         
         return make_response({'error': '401 Unauthorized'}, 401)
     
+class Users(Resource):
+
+    def get(self):
+
+        users = [user.to_dict() for user in User.query.all()]
+
+        if users:
+        
+            return make_response(jsonify(users), 200)
+        
+        return make_response({'error': '404 Not Found'}, 404)
+    
+    def post(self):
+
+        request_json = request.get_json()
+
+        username = request_json.get('username')
+        admin = request_json.get('admin')
+        password = request_json.get('password')
+
+        user = User(
+            username = username,
+            admin = admin
+        )
+
+        user.password_hash = password
+
+        try:
+
+            db.session.add(user)
+            db.session.commit()
+
+            return make_response(user.to_dict(), 201)
+    
+        except IntegrityError:
+
+            return make_response({'error': '422 Unprocessable Entity'}, 422)
+        
+class UsersByID(Resource):
+
+    def get(self, id):
+
+        user = User.query.filter(User.id == id).first()
+
+        if user:
+
+            return make_response(user.to_dict(), 200)
+        
+        return make_response({'error': '404 Not Found'}, 404)
+    
+    def patch(self, id):
+    
+        request_json = request.get_json()
+
+        username = request_json.get('username')
+        admin = request_json.get('admin')
+        password = request_json.get('password')
+
+        user = User.query.filter(User.id == id).first()
+
+        try: 
+            
+            user.username = username
+            user.admin = admin
+            user.password_hash = password
+
+            db.session.add(user)
+            db.session.commit()
+
+            return make_response(user.to_dict(), 200)
+        
+        except ValueError:
+            
+            return make_response({'error': '422 Unprocessable Entity'}, 422)
+        
+    def delete(self, id):
+
+        user = User.query.filter(User.id == id).first()
+
+        db.session.delete(user)
+        db.session.commit()
+
+        return make_response({}, 204)
+    
 
 api.add_resource(Login, '/login', endpoint='login')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
 api.add_resource(Logout, '/logout', endpoint='logout')
+api.add_resource(Users, '/users', endpoint='users')
+api.add_resource(UsersByID, '/users/<int:id>', endpoint='users/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)

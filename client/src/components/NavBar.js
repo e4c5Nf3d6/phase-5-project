f69caa2271d976d6ad3c6267fff01074f1f9438a
Maria Jenkins <macjenkin@gmail.com>
2024-04-18 09:56:35 -0500

    add NavBar styling

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -8,11 +8,41 @@ const linkStyles = {
     textAlign: "center",
     width: "100px",
     padding: "12px",
-    margin: "6px 6px 6px",
-    background: "white",
+    borderTop: "2px solid white",
+    borderBottom: "2px solid white",
+    borderRight: "2px solid white",
+    background: "#D3D3D3",
     textDecoration: "none",
     color: "black",
 };
+
+const firstLinkStyle = {
+    display: "inline-block",
+    textAlign: "center",
+    width: "100px",
+    padding: "12px",
+    border: "2px solid white",
+    borderTopLeftRadius: "15px",
+    borderBottomLeftRadius: "15px",
+    background: "#D3D3D3",
+    textDecoration: "none",
+    color: "black",
+}
+
+const lastLinkStyle = {
+    display: "inline-block",
+    textAlign: "center",
+    width: "100px",
+    padding: "12px",
+    borderTop: "2px solid white",
+    borderBottom: "2px solid white",
+    borderRight: "2px solid white",
+    borderTopRightRadius: "15px",
+    borderBottomRightRadius: "15px",
+    background: "#D3D3D3",
+    textDecoration: "none",
+    color: "black",
+}
 
 function NavBar() {
 
@@ -35,19 +65,34 @@ function NavBar() {
     }
     
     return (
-        <div className="navbar">
-            <NavLink 
-                to='/' 
-                exact
-                style={linkStyles}
-                activeStyle={{
-                    background: "#D3D3D3"
-                }}
-            >
-                Home
-            </NavLink>
-            <h2>{user.username}</h2>
-            <button onClick={handleLogout}>Logout</button>
+        <div id="navbar">
+            <div id="nav-locations">
+                <button>All Locations</button>
+                <button>Jones Valley</button>
+                <button>Midtown</button>
+                <button>Madison</button>
+            </div>
+            <div id="nav-main">
+                <h1>Color Order</h1>
+                <div id="nav-links">
+                    <NavLink 
+                        to="/" 
+                        exact
+                        style={firstLinkStyle}
+                        activeStyle={{
+                            background: "#5e7139",
+                            color: "white"
+                        }}
+                    >
+                        Home
+                    </NavLink>
+                </div>
+            </div>
+            <div id="nav-profile">
+                <h2>{user.username}</h2>
+                <button onClick={handleLogout}>Logout</button>
+            </div>
+
         </div>
     );
 }

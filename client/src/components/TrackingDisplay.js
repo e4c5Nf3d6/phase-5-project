a92608797c83bd31e859669a9ae4f72db13a9693
Maria Jenkins <macjenkin@gmail.com>
2024-04-23 10:45:35 -0500

    add date filter functionality

--- client/src/components/TrackingDisplay.js
+++ client/src/components/TrackingDisplay.js
@@ -5,16 +5,28 @@ import { selectAllProductOrders } from "../features/productOrders/productOrdersS
 function TrackingDisplay() {
 
     const productOrders = useSelector(selectAllProductOrders)
-    const query = useSelector((state) => state.productOrders.query)
+    const query = useSelector((state) => state.productOrders.query).toLowerCase()
     const category = useSelector((state) => state.productOrders.category)
+    const startDate = useSelector((state) => state.productOrders.startDate)
+    const endDate = useSelector((state) => state.productOrders.endDate)
 
     const filteredOrders = productOrders.filter((order) => {
+        const orderDate = order.order.date.split(" ")[0]
+        const productName = order.product.name.toLowerCase()
+        const orderCategory = order.product.category.name
+        
         if (category === null) {
-            return order.product.name.toLowerCase().includes(query.toLowerCase())
+            return productName.includes(query) && startDate <= orderDate && endDate >= orderDate
         } else {
-            return (order.product.category.name === category && order.product.name.toLowerCase().includes(query.toLowerCase()))
+            return orderCategory === category && productName.includes(query) && startDate <= orderDate && endDate >= orderDate
         }
     })
+
+    if (filteredOrders.length === 0) {
+        return (
+            <h1>No Products to Show</h1>
+        )
+    }
 
     return (
         <div className="display">
